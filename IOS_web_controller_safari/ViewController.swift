691aeeb4d66c932702ff1211fc69c4e9a62456da
Michael Lekrans <mlekrans@hotmail.com>
2019-07-11 10:59:16 +0200

    Initial Commit

---
+++ IOS_web_controller_safari/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IOS_web_controller_safari
+//
+//  Created by Michael Lekrans on 2019-07-11.
+//  Copyright © 2019 Michael Lekrans. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
